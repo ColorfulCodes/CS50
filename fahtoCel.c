d5fbd1b33ba0a472bf7c1a3f9663b60bba8b1ed6
ColorfulCodes <codescolorful@gmail.com>
2020-05-04 22:30:35 -0700

    feat: Reverse order of Fahrenheit to Celcius with for loop

--- fahtoCel.c
+++ fahtoCel.c
@@ -28,23 +28,33 @@ for fahr = 0,20,..... 300 */
 /* print Celsius-Fahrenheit table
 for fahr = 0,20,..... 300 */
 
-int main() 
+// int main() 
 
-{
-    float fahr, celsius;
-    int lower, upper, step;
+// {
+//     float fahr, celsius;
+//     int lower, upper, step;
 
-    lower = 0; /* lower limit of temperature*/
-    upper = 300; /* upper limit of temperature */
-    step = 20; /* step size */
+//     lower = 0; /* lower limit of temperature*/
+//     upper = 300; /* upper limit of temperature */
+//     step = 20; /* step size */
 
-    fahr = lower;
-    printf("Celcius - Fahrenheit\n");
-    printf("---------------------\n");
-    while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr -32.0);
-        printf("%6.1f %9.0f\n", celsius, fahr);
-        fahr = fahr + step;
+//     fahr = lower;
+//     printf("Celcius - Fahrenheit\n");
+//     printf("---------------------\n");
+//     while (fahr <= upper) {
+//         celsius = (5.0/9.0) * (fahr -32.0);
+//         printf("%6.1f %9.0f\n", celsius, fahr);
+//         fahr = fahr + step;
         
-    }
+//     }
+// }
+
+// Excercise 1-5: For loop in reverse order 
+
+int main()
+{
+    int fahr;
+
+    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+        printf( "%3d %6.1f\n",  fahr, (5.0/9.0) * (fahr -32.0));
 }
