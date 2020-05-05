dda049a499ab72b934c2157f78123eb036ea84da
ColorfulCodes <codescolorful@gmail.com>
2020-05-04 21:44:14 -0700

    refactor: turned ints to floats and learned spacing

--- fahtoCel.c
+++ fahtoCel.c
@@ -6,7 +6,7 @@ for fahr = 0,20,..... 300 */
 int main() 
 
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0; /* lower limit of temperature*/
@@ -14,9 +14,11 @@ int main()
     step = 20; /* step size */
 
     fahr = lower;
+    printf("Fahrenheit - Celcius\n");
+    printf("---------------------\n");
     while (fahr <= upper) {
-        celsius = 5* (fahr -32)/9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr -32.0);
+        printf("%5.0f %10.1f\n", fahr, celsius);
         fahr = fahr + step;
         
     }
