ccbd0707ce474c15413986d71a396e9842ae3cfc
ColorfulCodes <codescolorful@gmail.com>
2020-05-26 18:56:36 -0700

    feat: Binary Search in C

---
+++ binarySearch.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+
+/* binsearch: find x in v[0] <= v[1] <= ... <= v[n-1] */
+
+int binsearch(int x, int v[], int n)
+{
+    int low, high, mid;
+
+    low = 0;
+    high = n -1;
+    while (low <= high) {
+        mid = (low + high)/ 2;
+        if (x < v[mid]){
+            high = mid -1;
+        }
+        else if (x > v[mid]) {
+            low = mid + 1;
+        }
+        else{
+            // found match
+            return mid;
+        }
+        // no match
+        return -1;
+    }
+
+}
+
+int main()
+{
+    int arr[]={1,2,3,4,5,6,7,8};
+    int index = binsearch(4,arr,8);   
+    printf("Found at index: %d\n",index);
+}
