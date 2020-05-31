3a43a2d1f5856ac58b569a9837470d297f1712f7
ColorfulCodes <codescolorful@gmail.com>
2020-05-31 16:25:16 -0700

    refactor: clean up code and make more efficient

--- linkedList.c
+++ linkedList.c
@@ -30,25 +30,50 @@ void printlist(node_t *head) {
     printf("\n");
 }
 
-int main() {
-    node_t n1, n2, n3;
-    // declaring head pointer
-    node_t *head;
+//function that creates new nodes
 
-    n1.value = 45;
-    n2.value = 8;
-    n3.value = 32;
+//return node_t pointer
+// creat pointer to the new node
+node_t *create_new_node(int value) {
+    //create dynamically allocated pointer
+    // why second pointer?
+    node_t *result = malloc(sizeof(node_t));
+    result->value = value;
+    result->next = NULL;
+    return result;
+}
+
+//function to add node to the head of the list
+// pass a pointer to a pointer, head
+node_t *insert_at_head(node_t **head, node_t *node_to_insert) {
+    // point new node's next pointer to head
+    node_to_insert->next = *head;
+    *head = node_to_insert;
+    return node_to_insert;
+}
+
+int main() {
+    node_t *tmp;
+    // declaring head pointer
+    node_t *head = NULL;
+
+    for (int i = 0; i < 25; i++) {
+        tmp = create_new_node(i);
+        //de-reference head for value
+        head = insert_at_head(&head, tmp);
+    }
+    
 
     //link them up
 
     //make head point to the address of n3
-    head = &n3;
+    //head = &n3;
     // set each node's next pointer to the address of the other node
-    n3.next = &n2;
-    n2.next = &n1;
+    //n3.next = &n2;
+    //n2.next = &n1;
     // this is how we know when to stop.
-    n1.next = NULL;
-    head = head->next;
+    //n1.next = NULL;
+    //head = head->next;
     //call the function to print
     printlist(head);
     return 0;
